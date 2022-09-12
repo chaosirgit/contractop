8461efd1b3d58287a76a7919f81103642a15d6a8
郭超超 <chaosir@guochaochaodeMacBook-Pro.local>
2022-09-12 20:38:07 +0800

    初步完成

--- lib/controllers/OperationController.dart
+++ lib/controllers/OperationController.dart
@@ -1,4 +1,3 @@
-
 import 'dart:convert';
 import 'dart:io';
 
@@ -12,156 +11,171 @@ import 'package:web3dart/web3dart.dart';
 import 'package:http/http.dart';
 
 class OperationController extends GetxController with StateMixin {
-   final title = "".obs;
-   final methodName = "".obs; //方法名称
-   final color = Colors.orangeAccent.obs; //颜色
-   final storage = Get.find<StorageService>();
-   final RxList params = [].obs; //参数数组
-   final data = [].obs; //提交的参数
-   final payable = false.obs; //是否可以支付主币
-   final payValue = "0".obs; //支付的金额
-   final isLoading = false.obs; //loading
-   Map select = {}; //选择的方法
-   Map<String, Object?> op = {}; //用户
-   Map<String, Object?> c = {}; //合约
+  final title = "".obs;
+  final methodName = "".obs; //方法名称
+  final color = Colors.orangeAccent.obs; //颜色
+  final storage = Get.find<StorageService>();
+  final RxList params = [].obs; //参数数组
+  final data = [].obs; //提交的参数
+  final payable = false.obs; //是否可以支付主币
+  final payValue = "0".obs; //支付的金额
+  final isLoading = false.obs; //loading
+  Map select = {}; //选择的方法
+  Map<String, Object?> op = {}; //用户
+  Map<String, Object?> c = {}; //合约
+  late Client httpClient;
+  late Web3Client ethereumClient;
 
-   void getTitle() async {
-      var opId = await storage.box.read("operatorId");
-      var cId = await storage.box.read("contractId");
-      op = await Operator.find(opId);
-      c = await Contract.find(cId);
-      methodName.value = Get.arguments['methodName'];
-      color.value = Get.arguments['color'];
-      title.value = "Hi ${op['name']}, you operating (${c['name']}).$methodName now!";
-      var abiObj = parseAbi(c['abi'] as String);
-      ///读
-      if (color.value == Colors.blueAccent){
-         select = abiObj['read'].where((i) => i['name'] == methodName.value).toList().first;
-         params.value = select['inputs'];
+  void getTitle() async {
+    var opId = await storage.box.read("operatorId");
+    var cId = await storage.box.read("contractId");
+    op = await Operator.find(opId);
+    c = await Contract.find(cId);
+    data.value = [];
+    methodName.value = Get.arguments['methodName'];
+    color.value = Get.arguments['color'];
+    title.value =
+        "Hi ${op['name']}, you operating (${c['name']}).$methodName now!";
+    var abiObj = parseAbi(c['abi'] as String);
 
-      }else{
-         select = abiObj['send'].where((i) => i['name'] == methodName.value).toList().first;
-         if (select['stateMutability'] == "payable"){
-            payable.value = true;
-         }
-         params.value = select['inputs'];
+    ///读
+    if (color.value == Colors.blueAccent) {
+      select = abiObj['read']
+          .where((i) => i['name'] == methodName.value)
+          .toList()
+          .first;
+      params.value = select['inputs'];
+    } else {
+      select = abiObj['send']
+          .where((i) => i['name'] == methodName.value)
+          .toList()
+          .first;
+      if (select['stateMutability'] == "payable") {
+        payable.value = true;
       }
-      update();
-   }
+      params.value = select['inputs'];
+    }
+    update();
+  }
 
-   Future<myResponse> requestBlockChain() async {
-      isLoading.value = true;
-      ///建立web3
-      String rpc = "";
-      if (c['chain_id'] == 56){
-         rpc = "https://bsc-dataseed1.binance.org";
-      }else if (c['chain_id'] == 97) {
-         rpc = "https://data-seed-prebsc-1-s1.binance.org:8545";
-      }else if (c['chain_id'] == 1) {
-         rpc = "https://cloudflare-eth.com";
-      }
-      final client = Web3Client(rpc, Client());
-      try{
-         final credentials = await client.credentialsFromPrivateKey(decrypt(op['secret_key'] as String));
-         EtherAmount balance = await client.getBalance(credentials.address);
-         final ownAddress = await credentials.extractAddress();
-         final EthereumAddress contractAddr = EthereumAddress.fromHex(c['public_key'] as String);
-         final contract = DeployedContract(ContractAbi.fromJson(c['abi'] as String, 'MetaCoin'), contractAddr);
-         print(balance.getValueInUnit(EtherUnit.ether));
-         Map result = {};
-         ///格式化提交参数
-         var p = [];
-         for (var j = 0; j < data.value.length; j++){
-            if (data.value[j]['type'] == "uint256"){
-               p.add(BigInt.parse(data.value[j]['value']));
-            }else if (data.value[j]['type'] == "address"){
-               p.add(EthereumAddress.fromHex(data.value[j]['value']));
-            }else if (data.value[j]['type'] == "bool"){
-               if (data.value[j]['value'] == "true"){
-                  p.add(true);
-               }else if (data.value[j]['value'] == "false"){
-                  p.add(false);
-               }else{
-                  p.add(false);
-               }
-            }else if (data.value[j]['type'] == "string"){
-               p.add(data.value[j]['value']);
-            }else if (data.value[j]['type'] == "uint8") {
-               p.add(int.parse(data.value[j]['value']));
-            }else if (data.value[j]['type'] == "uint256[]"){
-               var str = data.value[j]['value'] as String;
-               var jde = str.replaceAll("[", "").replaceAll("]", "").split(",");
-               p.add(jde.map((e) => BigInt.parse(e)));
-            }else if (data.value[j]['type'] == "string[]"){
-               var str = data.value[j]['value'] as String;
-               var jde = str.replaceAll("[", "").replaceAll("]", "").split(",");
-               p.add(jde.map((e) => e));
-            }else if (data.value[j]['type'] == "address[]"){
-               var str = data.value[j]['value'] as String;
-               var jde = str.replaceAll("[", "").replaceAll("]", "").split(",");
-               p.add(jde.map((e) => print(e)));
-            }else if (data.value[j]['type'] == "uint8[]"){
-               var str = data.value[j]['value'] as String;
-               var jde = str.replaceAll("[", "").replaceAll("]", "").split(",");
-               p.add(jde.map((e) => int.parse(e)));
-            }
-            else{
-               p.add(data.value[j]['value']);
-            }
-         }
-         print(p);
+  Future<myResponse> requestBlockChain() async {
+    isLoading.value = true;
 
-         /// 读合约
-         if (select['stateMutability'] == "view"){
-            var res = await client.call(contract: contract, function: contract.function(select['name']), params: data.value);
-            List outputs = select['outputs'];
-            for (var i=0;i < outputs.length; i++){
-               result['name'] = outputs[i]['name'];
-               result['type'] = outputs[i]['type'];
-               result['value'] = res[i];
-            }
-         }else{
-            ///不支付 写合约
-            if (select['stateMutability'] == "nonpayable"){
-               var res = await client.sendTransaction(credentials, Transaction.callContract(
-                   contract: contract,
-                   function: contract.function(select['name']),
-                   parameters: data.value,
-               ));
-               List outputs = select['outputs'];
-               for (var i=0;i < outputs.length; i++){
-                  result['name'] = outputs[i]['name'];
-                  result['type'] = outputs[i]['type'];
-                  result['value'] = res[i];
-               }
-               ///支付
-            }else if (select['stateMutability'] == "payable"){
-               var res = await client.sendTransaction(credentials, Transaction.callContract(
-                  contract: contract,
-                  function: contract.function(select['name']),
-                  parameters: data.value,
-                  value: EtherAmount.fromUnitAndValue(EtherUnit.wei, payValue.value),
-               ));
-               List outputs = select['outputs'];
-               for (var i=0;i < outputs.length; i++){
-                  result['name'] = outputs[i]['name'];
-                  result['type'] = outputs[i]['type'];
-                  result['value'] = res[i];
-               }
-            }
-         }
-         print(result);
-         isLoading.value = false;
-         return myResponse.success(message: 'ok',data: result);
-      }catch (e) {
-         isLoading.value = false;
-         print(e);
-         return myResponse.error(message: "$e");
-      }finally{
-         client.dispose();
+    ///建立web3
+    String rpc = "";
+    if (c['chain_id'] == 56) {
+      rpc = "https://bsc-dataseed1.binance.org";
+    } else if (c['chain_id'] == 97) {
+      rpc = "https://data-seed-prebsc-1-s1.binance.org:8545";
+    } else if (c['chain_id'] == 1) {
+      rpc = "https://cloudflare-eth.com";
+    }
+    httpClient = Client();
+    ethereumClient = Web3Client(rpc, httpClient);
+    try {
+      final credentials =
+          EthPrivateKey.fromHex(decrypt(op['secret_key'] as String));
+      EtherAmount balance =
+          await ethereumClient.getBalance(credentials.address);
+      final ownAddress = await credentials.extractAddress();
+      final EthereumAddress contractAddr =
+          EthereumAddress.fromHex(c['public_key'] as String);
+      final contract = DeployedContract(
+          ContractAbi.fromJson(c['abi'] as String, c['name'] as String),
+          contractAddr);
+      //TODO 展示余额
+      //TODO 测试写方法
+      print(balance.getValueInUnit(EtherUnit.ether));
+      String response = "";
+
+      ///格式化提交参数
+      var p = [];
+
+      for (var j = 0; j < data.value.length; j++) {
+        if (data.value[j]['type'] == "uint256") {
+          p.add(BigInt.parse(data.value[j]['value']));
+        } else if (data.value[j]['type'] == "address") {
+          p.add(EthereumAddress.fromHex(data.value[j]['value']));
+        } else if (data.value[j]['type'] == "bool") {
+          if (data.value[j]['value'] == "true") {
+            p.add(true);
+          } else if (data.value[j]['value'] == "false") {
+            p.add(false);
+          } else {
+            p.add(false);
+          }
+        } else if (data.value[j]['type'] == "string") {
+          p.add(data.value[j]['value']);
+        } else if (data.value[j]['type'] == "uint8") {
+          p.add(BigInt.parse(data.value[j]['value']));
+        } else if (data.value[j]['type'] == "uint256[]") {
+          var str = data.value[j]['value'] as String;
+          var jde = str.replaceAll("[", "").replaceAll("]", "").split(",");
+          p.add(jde.map((e) => BigInt.parse(e)).toList());
+        } else if (data.value[j]['type'] == "string[]") {
+          var str = data.value[j]['value'] as String;
+          var jde = str.replaceAll("[", "").replaceAll("]", "").split(",");
+          p.add(jde.map((e) => e).toList());
+        } else if (data.value[j]['type'] == "address[]") {
+          var str = data.value[j]['value'] as String;
+          var jde = str.replaceAll("[", "").replaceAll("]", "").split(",");
+          p.add(jde
+              .map((e) => EthereumAddress.fromHex(
+                  e.replaceAll("'", "").replaceAll("\"", "")))
+              .toList());
+        } else if (data.value[j]['type'] == "uint8[]") {
+          var str = data.value[j]['value'] as String;
+          var jde = str.replaceAll("[", "").replaceAll("]", "").split(",");
+          p.add(jde.map((e) => BigInt.parse(e)).toList());
+        } else {
+          p.add(data.value[j]['value']);
+        }
       }
 
-   }
+      /// 读合约
+      if (select['stateMutability'] == "view") {
+        var res = await ethereumClient.call(
+            contract: contract,
+            function: contract.function(select['name']),
+            params: p);
+        response = res.toString();
+      } else {
+        ///不支付 写合约
+        if (select['stateMutability'] == "nonpayable") {
+          var res = await ethereumClient.sendTransaction(
+              credentials,
+              Transaction.callContract(
+                contract: contract,
+                function: contract.function(select['name']),
+                parameters: p,
+              ),
+              fetchChainIdFromNetworkId: true,
+              chainId: null);
+          response = res.toString();
 
-
+          ///支付
+        } else if (select['stateMutability'] == "payable") {
+          var res = await ethereumClient.sendTransaction(
+              credentials,
+              Transaction.callContract(
+                contract: contract,
+                function: contract.function(select['name']),
+                parameters: p,
+                value:
+                    EtherAmount.fromUnitAndValue(EtherUnit.wei, payValue.value),
+              ),
+              fetchChainIdFromNetworkId: true,
+              chainId: null);
+          response = res.toString();
+        }
+      }
+      isLoading.value = false;
+      return myResponse.success(message: 'ok', data: response);
+    } catch (e) {
+      isLoading.value = false;
+      return myResponse.error(message: "$e");
+    } finally {
+      ethereumClient.dispose();
+    }
+  }
 }
--- lib/view/OperationView.dart
+++ lib/view/OperationView.dart
@@ -4,7 +4,7 @@ import 'package:get/get.dart';
 import 'package:contractop/utils/constants.dart';
 import 'package:get/get_navigation/src/dialog/dialog_route.dart';
 
-class OperationView extends GetView<OperationController>{
+class OperationView extends GetView<OperationController> {
   final OperationController oc = Get.put(OperationController());
 
   @override
@@ -13,7 +13,6 @@ class OperationView extends GetView<OperationController>{
 
     /// 获取 title
     oc.getTitle();
-
 
     return Scaffold(
       appBar: AppBar(
@@ -30,54 +29,67 @@ class OperationView extends GetView<OperationController>{
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Obx(() => Padding(
-                padding: EdgeInsets.all(defaultPadding),
-                child: ElevatedButton.icon(
-                  icon: !oc.isLoading.value ?  Icon( Icons.send) :  CircularProgressIndicator(),
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor:oc.color.value,
-                      minimumSize : Size(160.0, 80.0),
-                  ),
-                  label: Text(!oc.isLoading.value ? "${oc.methodName}" : "Requesting..."),
-                  onPressed: () async {
-                    var res = await oc.requestBlockChain();
-                    if (res.code == 200) {
-                      return await Get.dialog(SimpleDialog(
-                          title: Text("${oc.methodName}"),
-                          titlePadding: EdgeInsets.all(defaultPadding),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(6))),
-                          children: [
-                            ListTile(title: Text("1"),subtitle: Text("mess"),),
-                          ],
-                      ));
-                    }else {
-                      return await Get.dialog(AlertDialog(title: Text("error"),content: Text(res.message),));
-                    }
+                    padding: const EdgeInsets.all(defaultPadding),
+                    child: ElevatedButton.icon(
+                      icon: !oc.isLoading.value
+                          ? const Icon(Icons.send)
+                          : const CircularProgressIndicator(),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: oc.color.value,
+                        minimumSize: const Size(160.0, 80.0),
+                      ),
+                      label: Text(!oc.isLoading.value
+                          ? "${oc.methodName}"
+                          : "Requesting..."),
+                      onPressed: () async {
+                        var res = await oc.requestBlockChain();
+                        if (res.code == 200) {
+                          return await Get.dialog(SimpleDialog(
+                            title: Text("${oc.methodName}"),
+                            titlePadding: const EdgeInsets.all(defaultPadding),
+                            shape: const RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(6))),
+                            children: [
+                              ListTile(
+                                title: const Text("OK"),
+                                subtitle: Text("${res.data}"),
+                              ),
+                            ],
+                          ));
+                        } else {
+                          return await Get.dialog(AlertDialog(
+                            title: const Text("Error"),
+                            content: Text(res.message),
+                          ));
+                        }
+                      },
+                    ),
+                  )),
 
-
-                  },
-                ),
-              )),
               /// 参数列表
               Expanded(
                 child: GetBuilder<OperationController>(builder: (_) {
                   List<Widget> list = [];
+
                   /// 声明相应的焦点变量
                   List<FocusNode> nods = [];
-                  for (var i = 0; i < oc.params.length; i++){
+                  for (var i = 0; i < oc.params.length; i++) {
                     nods.add(FocusNode());
-                    oc.data.add({"type":oc.params[i]['type'],"value":""});
+                    oc.data.add({"type": oc.params[i]['type'], "value": ""});
                   }
+
                   /// 列表
-                  for (var i = 0; i < oc.params.length; i++){
+                  for (var i = 0; i < oc.params.length; i++) {
                     String paramsName = oc.params[i]['name'];
                     list.add(TextField(
                       focusNode: nods[i],
+
                       /// 提交后切换焦点
                       onSubmitted: (d) {
                         nods[i].unfocus();
-                        if (i < oc.params.length - 1){
-                          FocusScope.of(context).requestFocus(nods[i+1]);
+                        if (i < oc.params.length - 1) {
+                          FocusScope.of(context).requestFocus(nods[i + 1]);
                         }
                       },
 
@@ -93,10 +105,10 @@ class OperationView extends GetView<OperationController>{
                           prefixIcon: Icon(Icons.abc)),
                     ));
                   }
-                  /// 需要支付
-                  if (oc.payable.value == true){
-                    list.add(TextField(
 
+                  /// 需要支付
+                  if (oc.payable.value == true) {
+                    list.add(TextField(
                       /// change 赋值
                       onChanged: (d) {
                         oc.payValue.value = d;
@@ -108,7 +120,9 @@ class OperationView extends GetView<OperationController>{
                           prefixIcon: Icon(Icons.currency_bitcoin)),
                     ));
                   }
-                   return ListView(children: list,);
+                  return ListView(
+                    children: list,
+                  );
                 }),
               ),
             ],
@@ -117,5 +131,4 @@ class OperationView extends GetView<OperationController>{
       ),
     );
   }
-
 }
